17eb7ac4cd3c3eeedf03a1815ed3b7d96b68a393
John Wynter <johnwynter55893@outlook.com>
2025-12-21 22:37:37 +1100

    Configure Next.js to ignore other projects in repository

--- Projects/Online psychic/next.config.js
+++ Projects/Online psychic/next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Ignore files outside the project directory
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  // Exclude other projects from the build
+  webpack: (config, { isServer }) => {
+    // Ignore files from Downloads and other project directories
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules/**',
+        '**/.next/**',
+        '**/Downloads/**',
+        '**/Projects/paw-or-naw/**',
+        '**/Projects/qut-lits-website/**',
+        '**/Projects/personal_portfolio/**',
+      ],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
